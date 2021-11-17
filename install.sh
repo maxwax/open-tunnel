748d3d23b35e40f67ec2108d9eea08160b49626d
Maxwell Spangler <maxcode@maxwellspangler.com>
2021-11-17 01:33:31 -0700

    Updated install file that creates symlink for open-terminal.

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 echo "Deploy open-tunnel to /usr/local/bin"
+
+# Deploy it
 sudo cp -pr open-tunnel /usr/local/bin
+
+# Make it executable
 sudo chmod a+rx /usr/local/bin/open-tunnel
+
+# Make a symlink to it as open-terminal so that command works too
+if [[ ! -L /usr/local/bin/open-terminal ]]
+then
+  ln -s /usr/local/bin/open-tunnel /usr/local/bin/open-terminal
+fi
+
+ls -l /usr/local/bin/open-tunnel
+ls -l /usr/local/bin/open-terminal
